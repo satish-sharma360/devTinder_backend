5cdc3f6df025f60b84328379c6a74a9b1fabee61
Satish Sharma <satisharma453@gmail.com>
2026-02-14 17:54:08 +0530

    connect with database

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/database.js';
+
 
 const app = express()
 
 let PORT = process.env.PORT || 7777;
 
-app.use('/hello',(req,res)=>{
-    res.send('Hellow from the helo route')
-})
-
 app.use('/',(req,res)=>{
     res.send('Hellow from the server')
 })
-app.use('/test',(req,res)=>{
-    res.send('Hellow from the test route')
-})
 
 
+await connectDB()
 app.listen(PORT,(req,res)=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
