4bcf024100f044fe9feec32a39fd628a208e6380
Satish Sharma <satisharma453@gmail.com>
2026-02-16 19:33:47 +0530

    login route completed with testing

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import validateUserdata from '../utils/validator.js';
-import { hashPassword } from '../service/passwordService.js';
+import { comparePassword, hashPassword } from '../service/passwordService.js';
 import userModel from '../models/user.model.js';
 import { generateToken } from '../service/tokenService.js';
 
@@ -23,7 +23,6 @@ authRouter.post('/signup', async (req, res) => {
         })
 
         const savedUser = await user.save()
-        console.log(savedUser)
 
         const token = await generateToken({id:savedUser._id})
 
@@ -34,6 +33,34 @@ authRouter.post('/signup', async (req, res) => {
         res.status(400).send("ERROR : " + error.message);
     }
 
+})
+
+authRouter.post('/login' , async (req,res) => {
+    try {
+        const {emailId , password} = req.body;
+
+        const user = await userModel.findOne({emailId:emailId})
+
+        if(!user){
+            throw new Error('Invalid credentials')
+        }
+
+        // compare pssword
+
+        const isMatch = await comparePassword(password, user.password)
+
+        if(!isMatch){
+            throw new Error('Invalid credentials')
+        }
+
+        const token = await generateToken({id:user._id})
+
+        res.cookie("token", token,{expires:new Date(Date.now() + 8 * 3600000)})
+
+        res.send(user)
+    } catch (error) {
+        res.status(400).send("ERROR : " + error.message);
+    }
 })
 
 export default authRouter;
